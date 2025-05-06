9fc1541eef5ba2051255a5b94885a59210b05641
Andrew Rowley <95974309+internetdrew@users.noreply.github.com>
2025-05-06 15:21:27 -0400

    add initial react router (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
-import { queryClient } from '../utils/trpc';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { trpc } from '../utils/trpc';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <h1>hello</h1>
-    </QueryClientProvider>
+  const { data, isLoading } = useQuery(
+    trpc.greeting.queryOptions({ name: 'John' })
   );
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return <h1>{data}</h1>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { queryClient } from '../utils/trpc';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    ),
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
