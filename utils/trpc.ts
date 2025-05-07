3895c57b9ffa8c5ea2a745ca5ecafa697ca00739
Andrew Rowley <andrew@internetdrew.com>
2025-05-06 23:20:06 -0400

    attempt vercel url

--- utils/trpc.ts
+++ utils/trpc.ts
@@ -1,12 +1,18 @@
-import { QueryClient } from '@tanstack/react-query';
-import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
-import type { AppRouter } from '../server';
-import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import { QueryClient } from "@tanstack/react-query";
+import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import type { AppRouter } from "../server";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
 
 export const queryClient = new QueryClient();
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return "http://localhost:3000"; // dev SSR should use localhost
+};
+
 const trpcClient = createTRPCClient<AppRouter>({
-  links: [httpBatchLink({ url: 'http://localhost:3000/trpc' })],
+  links: [httpBatchLink({ url: `${getBaseUrl()}/trpc` })],
 });
 
 export const trpc = createTRPCOptionsProxy<AppRouter>({
