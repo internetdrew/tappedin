2f8b6b90332710faaf5925222bc0a46bce63e3e6
Andrew Rowley <95974309+internetdrew@users.noreply.github.com>
2025-08-06 09:09:17 -0400

    add footer to site (#7)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="py-4">
+      <div className="container mx-auto px-4">
+        <p className="text-center text-sm">
+          An{" "}
+          <a href="https://internetdrew.com" className="text-pink-600">
+            InternetDrew
+          </a>{" "}
+          Experiment
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
